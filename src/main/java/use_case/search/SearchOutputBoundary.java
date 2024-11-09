030b100a908a11bc887508cdc21db60f198c92ef
ZheWang7 <jameswangzhe1110@gmail.com>
2024-11-09 15:29:43 -0500

    Implemented the interface and added TODOs

--- src/main/java/use_case/search/SearchOutputBoundary.java
+++ src/main/java/use_case/search/SearchOutputBoundary.java
@@ -4,4 +4,15 @@ package main.java.use_case.search;
  * Output boundary for the search use case.
  */
 public interface SearchOutputBoundary {
+    /**
+     * Prepares the success view for the Search use case.
+     * @param outputData the output data
+     */
+    void prepareSuccessView(SearchOutputData outputData);
+
+    /**
+     * Prepares the failure view for the Search use case.
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
 }
