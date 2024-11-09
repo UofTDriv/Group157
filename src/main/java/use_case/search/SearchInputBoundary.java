037a3a31685d46ac114311ba97d3ad211eb64ff9
ZheWang7 <jameswangzhe1110@gmail.com>
2024-11-09 15:24:57 -0500

    Implemented the interface and added TODOs

--- src/main/java/use_case/search/SearchInputBoundary.java
+++ src/main/java/use_case/search/SearchInputBoundary.java
@@ -4,4 +4,10 @@ package main.java.use_case.search;
  * Input Boundary for the search use case.
  */
 public interface SearchInputBoundary {
+    /**
+     * Executes the search use case: update the SearchPresenter to show the corresponding
+     * webpage given the input data, or failure info if the input is invalid.
+     * @param inputData the input data
+     */
+    void execute(SearchInputData inputData);
 }
--- src/main/java/use_case/search/SearchInteractor.java
+++ src/main/java/use_case/search/SearchInteractor.java
@@ -4,4 +4,14 @@ package main.java.use_case.search;
  * Interactor for the search function.
  */
 public class SearchInteractor implements SearchInputBoundary {
+    private final SearchOutputBoundary presenter;
+
+    public SearchInteractor(SearchOutputBoundary presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void execute(SearchInputData inputData) {
+        // TODO: Implement the execute method. This should use the MediaWiki API.
+    }
 }
