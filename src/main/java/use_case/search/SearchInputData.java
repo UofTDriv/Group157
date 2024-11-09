d95473a248635591cebe7f9df13f505f7aea34b4
ZheWang7 <jameswangzhe1110@gmail.com>
2024-11-09 15:32:12 -0500

    Implemented the SearchInputData class.

--- src/main/java/use_case/search/SearchInputData.java
+++ src/main/java/use_case/search/SearchInputData.java
@@ -4,4 +4,14 @@ package main.java.use_case.search;
  * Input data for the search use case.
  */
 public class SearchInputData {
+
+    private final String searchString;
+
+    public SearchInputData(String searchString) {
+        this.searchString = searchString;
+    }
+
+    public String getSearchString() {
+        return searchString;
+    }
 }
