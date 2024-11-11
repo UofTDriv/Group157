7ae032e8d8dbf0cca5199e6a84d7d786669db7b7
Hrunt J <emmak@Emmas-MacBook-Air-109.local>
2024-11-11 16:11:50 -0500

    Some package name changes in SearchInputBoundary.java and SearchInputData.java - Created the WebPage entity and implemented the SearchController

--- src/main/java/entity/WebPage.java
+++ src/main/java/entity/WebPage.java
@@ -4,4 +4,12 @@ package main.java.entity;
  * Stores the data representing a webpage.
  */
 public class WebPage {
+
+    public final String title;
+    public final String content;
+
+    public WebPage(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
