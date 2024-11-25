7dd232e58dd5e066149eaf3bd6cf8df2c5a571b2
Hrunt J <emmak@Emmas-MacBook-Air-109.local>
2024-11-25 11:11:59 -0500

    Implementation of Save Use case from Controller to Presenter

--- src/main/java/entity/WikiHistory.java
+++ src/main/java/entity/WikiHistory.java
@@ -1,4 +1,11 @@
 package entity;
 
 public class WikiHistory {
+    public String title;
+    public Journey journey;
+
+    public WikiHistory(String title, Journey journey) {
+        this.title = title;
+        this.journey = journey;
+    }
 }
--- src/main/java/use_case/save/SaveDataAccessInterface.java
+++ src/main/java/use_case/save/SaveDataAccessInterface.java
@@ -1,4 +1,25 @@
 package use_case.save;
 
+import entity.Journey;
+import entity.WikiHistory;
+
 public interface SaveDataAccessInterface {
+    /**
+     * Checks if a given wikiHistory exists in the hash map or DB
+     * @param title the title of the wikiHistory
+     * @return true iff the wikiHistory exists in the hash map or DB
+     */
+    boolean wikiHistoryExists(String title);
+
+    /**
+     * Saves wikiHistory into the hash map or DB, assuming that it does not already exist
+     * @param wikiHistory the journey object to be saved
+     */
+    void save(WikiHistory wikiHistory) throws Exception;
+
+    /**
+     * Gets the current wikiHistory object
+     * @return The current wikiHistory object
+     */
+    WikiHistory getCurrentWikiHistory();
 }
